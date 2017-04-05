65ea46183e01cc4a0177e5144321904489ea99cb
Valentina <valya.pradun@gmail.com>
2017-04-05 11:20:50 +0300

    Delete SortException.java, exception handling

--- src/com/epam/training/java/sort/Service.java
+++ src/com/epam/training/java/sort/Service.java
@@ -6,26 +6,18 @@ public class Service {
 	}
 
 	public static void printArray(int[] array) {
-		if (array == null) {
-			System.out.println("Error: You sent the empty array!");
-		} else {
-			System.out.print("Array = ");
-			for (int i : array) {
-				System.out.print(i + " ");
-			}
-			System.out.println("");
+		System.out.print("Array = ");
+		for (int i : array) {
+			System.out.print(i + " ");
 		}
+		System.out.println("");
 	}
 
 	public void bubleSort() {
 		System.out.println("Sorting by a bubble:");
 		int[] array = { 17, 2, -5, 6, 0, 8, 17, -20 };
 		printArray(array);
-		try {
-			printArray(Sort.sortBubble(array));
-		} catch (SortException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
+		printArray(Sort.sortBubble(array));
 		System.out.println("");
 	}
 
@@ -33,11 +25,7 @@ public class Service {
 		System.out.println("Sorting by the choice:");
 		int[] array = { -4, 27, 15, -2, 63, 18, 46, 100 };
 		printArray(array);
-		try {
-			printArray(Sort.sortSelection(array));
-		} catch (SortException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
+		printArray(Sort.sortSelection(array));
 		System.out.println("");
 	}
 
@@ -45,11 +33,7 @@ public class Service {
 		System.out.println("Sorting by inserts:");
 		int[] array = { 16, -1, 83, 10, -1, 5, 25, 33 };
 		printArray(array);
-		try {
-			printArray(Sort.sortInsertion(array));
-		} catch (SortException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
+		printArray(Sort.sortInsertion(array));
 		System.out.println("");
 	}
 
@@ -57,11 +41,7 @@ public class Service {
 		System.out.println("Quick sort:");
 		int[] array = { 25, 15, -1, 69, 4, 37, -9, 0 };
 		printArray(array);
-		try {
-			printArray(Sort.sortQuick(array));
-		} catch (SortException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
+		printArray(Sort.sortQuick(array));
 		System.out.println("");
 	}
 
--- src/com/epam/training/java/sort/Sort.java
+++ src/com/epam/training/java/sort/Sort.java
@@ -1,68 +1,56 @@
 package com.epam.training.java.sort;
 
-public class Sort {
+public final class Sort {
 
-	public static int[] sortBubble(int[] array) throws SortException {
-		if (array == null) {
-			throw new SortException("You sent the empty array!");
-		} else {
-			for (int i = array.length - 1; i > 0; i--) {
-				for (int j = 0; j < i; j++) {
-					if (array[j] > array[j + 1]) {
-						int tmp = array[j];
-						array[j] = array[j + 1];
-						array[j + 1] = tmp;
-					}
+	private Sort() {
+
+	}
+
+	public static int[] sortBubble(int[] array) {
+		for (int i = array.length - 1; i > 0; i--) {
+			for (int j = 0; j < i; j++) {
+				if (array[j] > array[j + 1]) {
+					int tmp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = tmp;
 				}
 			}
 		}
 		return array;
 	}
 
-	public static int[] sortSelection(int[] array) throws SortException {
-		if (array == null) {
-			throw new SortException("You sent the empty array!");
-		} else {
-			for (int i = 0; i < array.length - 1; i++) {
-				int min = i;
-				for (int j = i + 1; j < array.length; j++) {
-					if (array[j] < array[min]) {
-						min = j;
-					}
+	public static int[] sortSelection(int[] array) {
+		for (int i = 0; i < array.length - 1; i++) {
+			int min = i;
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[j] < array[min]) {
+					min = j;
 				}
-				int tmp = array[i];
-				array[i] = array[min];
-				array[min] = tmp;
+			}
+			int tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+		}
+		return array;
+	}
+
+	public static int[] sortInsertion(int[] array) {
+		for (int i = 1; i < array.length; i++) {
+			int current = array[i];
+			int previous = i - 1;
+			while (previous >= 0 && array[previous] > current) {
+				array[previous + 1] = array[previous];
+				array[previous] = current;
+				previous--;
 			}
 		}
 		return array;
 	}
 
-	public static int[] sortInsertion(int[] array) throws SortException {
-		if (array == null) {
-			throw new SortException("You sent the empty array!");
-		} else {
-			for (int i = 1; i < array.length; i++) {
-				int current = array[i];
-				int previous = i - 1;
-				while (previous >= 0 && array[previous] > current) {
-					array[previous + 1] = array[previous];
-					array[previous] = current;
-					previous--;
-				}
-			}
-		}
-		return array;
-	}
-
-	public static int[] sortQuick(int[] array) throws SortException {
-		if (array == null) {
-			throw new SortException("You sent the empty array!");
-		} else {
-			int start = 0;
-			int end = array.length - 1;
-			sort(start, end, array);
-		}
+	public static int[] sortQuick(int[] array) {
+		int start = 0;
+		int end = array.length - 1;
+		sort(start, end, array);
 		return array;
 	}
 
